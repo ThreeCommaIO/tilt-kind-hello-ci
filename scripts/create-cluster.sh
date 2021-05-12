0cbc0a7c0b77fca326f4acb19a0ba411ee4459bd
Chris Goffinet <chris@threecomma.io>
2021-05-12 00:30:05 -0400

    more fixes for local + remote builds

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -4,13 +4,14 @@ set -ex
 
 if [[ $CIRCLE_SHELL_ENV == *"localbuild"* ]]; then
   export DOCKER_HOST=unix:///var/run/docker.sock
+  # does a local kind cluster already exist?
+  kind export kubeconfig || true
+
+  if ctlptl get cluster kind-kind; then
+    PORT=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " " | awk '{split($0,a,":"); print a[3]}')
+    ctlptl socat connect-remote-docker "$PORT"
+  fi
 fi
 
-if ! ctlptl get cluster kind-kind; then
-  ctlptl apply -f registry.yaml
-  ctlptl apply -f cluster.yaml
-else
-  kind export kubeconfig
-  PORT=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " " | awk '{split($0,a,":"); print a[3]}')
-  ctlptl socat connect-remote-docker "$PORT"
-fi
+ctlptl apply -f registry.yaml
+ctlptl apply -f cluster.yaml
