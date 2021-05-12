5fc1c765d67a3acd97c9560327409a8b957b889e
Chris Goffinet <chris@threecomma.io>
2021-05-12 00:14:12 -0400

    move kind export only if cluster exists

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -6,12 +6,11 @@ if [[ $CIRCLE_SHELL_ENV == *"localbuild"* ]]; then
   export DOCKER_HOST=unix:///var/run/docker.sock
 fi
 
-kind export kubeconfig
-
 if ! ctlptl get cluster kind-kind; then
   ctlptl apply -f registry.yaml
   ctlptl apply -f cluster.yaml
 else
+  kind export kubeconfig
   PORT=$(kubectl config view --minify | grep server | cut -f 2- -d ":" | tr -d " " | awk '{split($0,a,":"); print a[3]}')
   ctlptl socat connect-remote-docker "$PORT"
 fi
